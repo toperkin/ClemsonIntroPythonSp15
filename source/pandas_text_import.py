6125d8c67e346c775c3bfa1b2005f2e03b536159
toperkin <toperkin@gmail.com>
2015-01-31 14:42:18 -0600

    up

--- source/pandas_text_import.py
+++ source/pandas_text_import.py
@@ -1,38 +1,36 @@
-__author__ = 'tony'
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-A = pd.read_table("band.data", sep=" ")
-A = A.dropna(axis=1, how='all')
+data = pd.read_table("band.data", sep=" ", header=None)
+data = data.dropna(axis=1, how='all')
+plt.figure()
+data.plot()
+plt.savefig('pd_text_import1')
 
-print(A)
 
-"""
-genfromtxt reads in line by line. so the array is arranged by rows.
-Swapping it out with the transpose is an easy way to switch the rows and columns.
-"""
+plt.figure()
+data.plot(x=2)
+plt.savefig('pd_text_import2')
 
-'''
-A = A.T
+def scale(x):
+    return 27.21*x-0.3
 
-fig = plt.figure()
+data[1:] = data[1: ].apply(scale)
 
-for k in range(1, len(A)):
-    plt.plot(A[0], 27.21*A[k]-0.3)
+plt.figure()
+data.plot(x=2, legend=False)
+plt.savefig('pd_text_import3')
 
-plt.title('Band')
-plt.xlim([A[0].min(), A[0].max()])
-fig.savefig("numpy_graphing1.png")
 
-fig2 = plt.figure()
+plt.figure()
+i=0
+for col in data.columns:
+    if col != 2:
+        if data[col].min() > -2 and data[col].max() < 3:
+            if i==0:
+                ax = data.plot(x=2, y=col, legend=False)
+                i=1
+            else:
+                data.plot(x=2, y=col, legend=False, ax=ax)
 
-for k in range(1, len(A)):
-    if min( 27.21*A[k]-0.3 ) > -2 and max( 27.21*A[k]-0.3 ) < 3:
-        plt.plot(A[0], 27.21*A[k]-0.3)
-
-plt.title('Band')
-plt.xlim([A[0].min(), A[0].max()])
-
-fig2.savefig("numpy_graphing2.png")
-'''
+plt.savefig('pd_text_import4')
